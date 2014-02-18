dcbb62dc012722b3e437e929d5d4ec6b49f3a17d
Lau Ching Jun <lauchingjun@hotmail.com>
2014-02-18 18:46:41 +0800

    Add true and false

---
+++ applets/_false.rs
@@ -0,0 +1,5 @@
+use std;
+pub fn main(_: &[~str]) {
+    std::os::set_exit_status(1);
+}
+
--- applets/mod.rs
+++ applets/mod.rs
@@ -2,12 +2,14 @@ mod cat;
 mod clear;
 mod dirname;
 mod echo;
+mod _false;
 mod head;
 mod mkdir;
 mod pwd;
 mod rmdir;
 mod sleep;
 mod tee;
+mod _true;
 mod wc;
 mod which;
 mod yes;
@@ -18,12 +20,14 @@ pub fn find_applet(name: &str) -> Option<fn(&[~str])> {
         "clear" => Some(clear::main),
         "dirname" => Some(dirname::main),
         "echo" => Some(echo::main),
+        "false" => Some(_false::main),
         "head" => Some(head::main),
         "mkdir" => Some(mkdir::main),
         "pwd" => Some(pwd::main),
         "rmdir" => Some(rmdir::main),
         "sleep" => Some(sleep::main),
         "tee" => Some(tee::main),
+        "true" => Some(_true::main),
         "wc" => Some(wc::main),
         "which" => Some(which::main),
         "yes" => Some(yes::main),
